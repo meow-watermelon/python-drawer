bc550d0648114ee98577c63f7dea41eceeef1ee2
Hui Li <herdingcat@yahoo.com>
2022-03-20 23:20:07 -0700

    add parameters for decorator
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- retry-decorator.py
+++ retry-decorator.py
@@ -4,33 +4,38 @@ import functools
 import requests
 import time
 
-def retry_wrapper(func):
-    @functools.wraps(func)
-    def retry_logic(*args, **kargs):
-        retry = 5
-        gap = 0
-        n = 0
+def retry_wrapper(_func=None, *, retry=5):
+    def retry_logic_wrapper(func):
+        @functools.wraps(func)
+        def retry_logic(*args, **kargs):
+            gap = 0
+            n = 0
+            while True:
+                if n == retry:
+                    print("exhausted. exited.")
+                    break
+                rc = func(*args, **kargs)
+                if rc != 200:
+                    n += 1
+                    sleep_time = 2 ** gap
+                    gap += 1
+                    print("sleep %d seconds for the %d retry - rc: %d" %(sleep_time, n, rc))
+                    time.sleep(sleep_time)
+                else:
+                    print("status_code: %d" %(rc))
+                    break
+            return rc
+        return retry_logic
 
-        while True:
-            if n == retry:
-                print("exhausted. exited.")
-                break
-            rc = func(*args, **kargs)
-            if rc != 200:
-                n += 1
-                sleep_time = 2 ** gap
-                gap += 1
-                print("sleep %d seconds for the %d retry - rc: %d" %(sleep_time, n, rc))
-                time.sleep(sleep_time)
-            else:
-                print("status_code: %d" %(rc))
-                break
-        return func(*args, **kargs)
-    return retry_logic
+    if _func is None:
+        return retry_logic_wrapper
+    else:
+        return retry_logic_wrapper(_func)
                 
-@retry_wrapper
+@retry_wrapper(retry=3)
 def get_status_code(url):
     r = requests.get(url)
     return r.status_code
 
-get_status_code('https://www.pinterest.com/god-damn-i-know-this-does-not-exist')
+print(get_status_code('https://www.pinterest.com/god-damn-i-know-this-does-not-exist'))
+print(get_status_code('https://www.pinterest.com'))
