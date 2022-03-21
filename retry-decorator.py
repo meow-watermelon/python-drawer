9bf548e0952f7bedd639febf837e2cd719158615
Hui Li <herdingcat@yahoo.com>
2022-03-20 20:10:49 -0700

    a retry decorator
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

---
+++ retry-decorator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import functools
+import requests
+import time
+
+def retry_wrapper(func):
+    @functools.wraps(func)
+    def retry_logic(*args, **kargs):
+        retry = 5
+        gap = 0
+        n = 0
+
+        while True:
+            rc = func(*args, **kargs)
+            if rc != 200:
+                n += 1
+                if n == retry + 1:
+                    print("exhausted. exited.")
+                    break
+                sleep_time = 2 ** gap
+                gap += 1
+                print("sleep %d seconds for the %d retry - rc: %d" %(sleep_time, n, rc))
+                time.sleep(sleep_time)
+            else:
+                print("status_code: %d" %(rc))
+                break
+        return func(*args, **kargs)
+    return retry_logic
+                
+@retry_wrapper
+def get_status_code(url):
+    r = requests.get(url)
+    return r.status_code
+
+get_status_code('https://www.pinterest.com/god-damn-i-know-this-does-not-exist')
