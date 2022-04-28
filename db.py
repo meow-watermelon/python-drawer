5dec5403b04803fbaf1ed1ba582bfe3f6c9b5c39
Hui Li <herdingcat@yahoo.com>
2022-04-27 20:47:00 -0700

    fix execute() method with default value

--- db.py
+++ db.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import logging
+import os
 import sqlite3
 import sys
+
 
 class SQLiteDBConnectException(Exception):
     pass
 
+
 class SQLiteDBExecuteException(Exception):
     pass
 
-class DBClient():
+
+class DBClient:
     def __init__(self, db_name):
         self.db_name = db_name
         self.db_connection = None
@@ -20,18 +24,21 @@ class DBClient():
         try:
             self.db_connection = sqlite3.connect(self.db_name)
         except sqlite3.OperationalError as e:
-            #logging.exception(e)
             raise SQLiteDBConnectException(str(e))
 
     def cursor(self):
         self.db_cursor = self.db_connection.cursor()
 
-    def execute(self, sql_executable):
+    def execute(self, sql_executable, parameters=None):
+        if parameters is None:
+            parameters = ()
+
         try:
-            self.db_cursor.execute(sql_executable)
+            self.db_cursor.execute(sql_executable, parameters)
         except sqlite3.OperationalError as e:
-            #logging.exception(e)
             raise SQLiteDBExecuteException(str(e))
+
+        return self.db_cursor
 
     def commit(self):
         self.db_connection.commit()
@@ -39,7 +46,26 @@ class DBClient():
     def close(self):
         self.db_connection.close()
 
-db_name = 'test.db'
+
+dogs = {
+    "alpha": {
+        "color": "black",
+        "address": "USA",
+    },
+    "beta": {
+        "color": "red",
+        "address": "CANADA",
+    },
+    "gamma": {
+        "color": "brown",
+        "address": "UK",
+    },
+}
+
+db_name = "test.db"
+
+if os.path.exists(db_name):
+    os.remove(db_name)
 
 db = DBClient(db_name)
 
@@ -47,7 +73,7 @@ try:
     db.connect()
 except SQLiteDBConnectException as e:
     logging.exception(e)
-    print(f'failed to initialize the db connection: {e}', file=sys.stderr)
+    print(f"failed to initialize the db connection: {e}", file=sys.stderr)
     sys.exit(2)
 
 db.cursor()
@@ -56,14 +82,22 @@ try:
     db.execute("CREATE TABLE snoopy (name TEXT, color TEXT, address TEXT)")
 except SQLiteDBExecuteException as e:
     logging.exception(e)
-    print(f'failed to create table: {e}', file=sys.stderr)
+    print(f"failed to create table: {e}", file=sys.stderr)
     sys.exit(2)
 
-try:
-    db.execute("INSERT INTO snoopy VALUES ('mydog', 'brown', 'CA')")
-except SQLiteDBExecuteException as e:
-    logging.exception(e)
-    print(f'failed to run sql executable: {e}', file=sys.stderr)
+for name in dogs.keys():
+    color = dogs.get(name).get("color")
+    address = dogs.get(name).get("address")
+
+    try:
+        db.execute("INSERT INTO snoopy VALUES (?, ?, ?)", (name, color, address))
+    except SQLiteDBExecuteException as e:
+        logging.exception(e)
+        print(f"failed to run sql executable: {e}", file=sys.stderr)
 
 db.commit()
+
+for row in db.execute("SELECT * FROM snoopy"):
+    print(row)
+
 db.close()
